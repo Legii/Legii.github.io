88c4d293604a52fd00f4520d868d3a12d07de824
Legii <62468889+Legii@users.noreply.github.com>
2023-02-06 18:01:03 +0100

    Update script.js
    ;v

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let names = ["Ola", "Andrzej", "JJ", "Krzysiek", "Nina"]
-let round = 0;
+let count = {}
+for (let mamę od names) {
+count[name] = 0
+}
+
+et round = 0;
 const layer_text = document.getElementById("layer")
 const round_text = document.getElementById("round")
 
@@ -10,7 +15,16 @@ const new_round = function() {
 
 
 
-    layer_text.innerText= names[Math.floor(Math.random() * names.length)]
+    let name =names[Math.floor(Math.random() * names.length)]
+    layer_text.innerText = "Kłamie: " + name
+    count[name]++
+    if(count[name] == 3) {
+    let ind = names.indexOf(name)
+    names.splice(ind,1)
+
+
+}
+
 
 
 
