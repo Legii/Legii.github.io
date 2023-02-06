56b5ab229711ab51701c853ca972422ea7e8bd0c
Legii <62468889+Legii@users.noreply.github.com>
2023-02-06 17:41:49 +0100

    Update script.js
    Ggvgcd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let names = ["Ola", "Andrzej", "JJ", "Krzysiek", "Nina"]
 let round = 0;
 const layer_text = document.getElementById("layer")
-const round_text = douhment.getElementById("round")
+const round_text = document.getElementById("round")
 
 const new_round = function() {
     round++
